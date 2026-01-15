418787a904f13062075796211589f99f3602a7a5
Velin Dankov <velin80@web.de>
2026-01-15 08:29:15 +0200

    adding remove item from basket

--- src/app/products/shopping-cart/cart-item/cart-item.component.ts
+++ src/app/products/shopping-cart/cart-item/cart-item.component.ts
@@ -1,6 +1,7 @@
-import { Component, input } from "@angular/core";
+import { Component, inject, input } from "@angular/core";
 import { Product } from "../../../product.model.js";
 import { CurrencyPipe } from "@angular/common";
+import { CardServiceService } from "../../../card-service.service.js";
 
 @Component({
   selector: "app-cart-item",
@@ -10,4 +11,9 @@ import { CurrencyPipe } from "@angular/common";
 })
 export class CartItemComponent {
   item = input.required<Product>();
+  private cartService = inject(CardServiceService);
+
+  onRemoveItem() {
+    this.cartService.removeItem(this.item().id);
+  }
 }
