e5a6363732eabce14d6a7b94ff3c9b41db7183ae
Velin Dankov <velin80@web.de>
2026-01-15 08:38:43 +0200

    show total price in shopping cart

--- src/app/products/shopping-cart/shopping-cart.component.ts
+++ src/app/products/shopping-cart/shopping-cart.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from "@angular/core";
 import { CardServiceService } from "../../card-service.service.js";
 import { CartItemComponent } from "./cart-item/cart-item.component.js";
+import { CurrencyPipe } from "@angular/common";
 
 @Component({
   selector: "app-shopping-cart",
-  imports: [CartItemComponent],
+  imports: [CartItemComponent, CurrencyPipe],
   templateUrl: "./shopping-cart.component.html",
   styleUrl: "./shopping-cart.component.scss",
 })
